f991c016f7e1db26270b5ac6725ba3135380e923
matthcol <matthias.colin@gmail.com>
2025-03-10 13:16:14 +0100

    lundi matin

---
+++ basics/src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+// la classe principale : fichier de démarrage
+public class Main {
+
+    // point d'entrée du programme
+    public static void main(String[] args) {
+        // Texte en Unicode: https://en.wikipedia.org/wiki/Unicode
+        System.out.println("Hello tout le monde");
+        System.out.println("On commence l'apprentissage du langage Java:");
+
+        // Texte Multi-line: Java 17 (introduced in Java 15)
+        System.out.println("""
+                * Lundi: les bases
+                * Mardi: les fonctions
+                * Mercredi: les objets
+                
+                Je dis: "Java c'est bien"
+                """);
+        System.out.println("Je dis: \"Java c'est bien\"");
+        System.out.println("Mots en français: été, cœur, L'Haÿ-les-Roses, L'HAŸ-LES-ROSES");
+        System.out.println("東京");
+        System.out.println("🦜❤️");
+    }
+}
